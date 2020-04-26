c426cc0ce7afcbed41f4713e94cae552d5187860
unknown <cacup7@gmail.com>
2020-04-25 21:59:39 -0300

    Conteúdos armazenados no main

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react';
+import api from '../services/api';
 
 class Main extends Component {
+  state = {
+    data: [],
+  }
+
+  componentDidMount() {
+     this.loadPosts();
+  }
+
+  async loadPosts() {
+    let res = await api.get();
+    this.setState({data: res.data});
+  }
+
   render() {
     return (
       <main id="conteudo-principal">Texto do conteudo principal</main>
